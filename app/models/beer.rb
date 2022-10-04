263274838ef4933643a3a81f62a1d34227d3dc10
Petro Lehtonen <petro.lehtonen@gmail.com>
2022-10-04 11:10:40 +0300

    viikko 2 teht. 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,13 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    avg = 0
+    self.ratings.each {|rating|
+        avg = avg + rating.score
+    }
+    (avg / self.ratings.count).to_f
+  end
+
 end
