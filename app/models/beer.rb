9a74c96491633e2b3ae3b7fe7d58f4b6f30b55ec
Petro Lehtonen <petro.lehtonen@gmail.com>
2022-10-14 19:45:21 +0300

    viikko 2 teht. 8-13

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,9 +1,13 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
 
   def average_rating
     self.ratings.average(:score)
+  end
+
+  def to_s
+    "#{self.name}, #{self.brewery.name}"
   end
 
 end
