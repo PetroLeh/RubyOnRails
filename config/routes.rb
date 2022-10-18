d2e2eb472d7c894b686a484a9c78215db927f571
Petro Lehtonen <petro.lehtonen@gmail.com>
2022-10-18 10:06:08 +0300

    viikko 3 teht. 1-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
+  resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   
-  root 'breweries#index'
-  
-  #get 'ratings', to: 'ratings#index'
-  #get 'ratings/new', to: 'ratings#new'  
-  #post 'ratings', to: 'ratings#create'
+  get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
-  resources :ratings, only: [:index, :new, :create, :destroy]
+  root 'breweries#index'
 end
