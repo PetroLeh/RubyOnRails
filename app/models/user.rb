742ba031a1cfc974a37182eb91d87c9cc53b53c4
Petro Lehtonen <petro.lehtonen@gmail.com>
2022-10-26 11:19:24 +0300

    viikko3 teht. 9-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
   validates :username, uniqueness: true,
                        length: { in: 3..30 }
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
+  has_many :memberships, dependent: :destroy
+  has_many :beer_clubs, through: :memberships
+
 end
