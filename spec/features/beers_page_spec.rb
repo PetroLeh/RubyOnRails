81964261befbb0311403588da65a2a59f5117b1b
Petro Lehtonen <petro.lehtonen@gmail.com>
2022-10-31 12:30:05 +0200

    viikko 4 teht. 5 täydennys

--- spec/features/beers_page_spec.rb
+++ spec/features/beers_page_spec.rb
@@ -8,14 +8,23 @@ describe "Beer" do
        brewery = FactoryBot.create(:brewery)
     end
 
-   it "can be created" do
+   it "with valid name can be created" do
     visit beers_path
     click_link("New beer")
 
-    fill_in("beer_name", with: "Great Beer")
-    save_and_open_page
-    
+    fill_in("beer_name", with: "Great Beer")    
     expect{ click_button("Create Beer") }.to change{Beer.count}.by(1)
    end
+
+   it "with invalid name is not created and gives proper error message" do
+    visit beers_path
+    click_link("New beer")
+
+    beer_count = Beer.count    
+    click_button("Create Beer")
+    expect(page).to have_content("Name can't be blank")
+    expect(Beer.count).to eq(beer_count)
+   end 
+
 end
 
