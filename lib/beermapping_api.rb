ebc0dae2fba6b4195665347e60626909cc667b38
Petro Lehtonen <petro.lehtonen@gmail.com>
2022-11-04 12:05:22 +0200

    välimuistin käyttöönotto ja tyylin siivous

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,19 +1,30 @@
 class BeermappingApi
-    def self.places_in(city)
-      url = "http://beermapping.com/webservice/loccity/#{key}/"
-  
-      response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
-      places = response.parsed_response["bmp_locations"]["location"]
-  
-      return [] if places.is_a?(Hash) and places["id"].nil?
-  
-      places = [ places ] if places.is_a?(Hash)
-      places.map do | place |
-        Place.new(place)
-      end
+  def self.places_in(city)
+    city = city.downcase
+
+    places = Rails.cache.read(city)
+    return places if places
+
+    places = get_places_in(city)
+    Rails.cache.write(city, places)
+    places
+  end
+
+  def self.get_places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places.is_a?(Hash) && places["id"].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.map do |place|
+      Place.new(place)
     end
-  
-    def self.key
-      "843f65e501d2d83febd59fea1e727d38"
-    end
+  end
+
+  def self.key
+    "843f65e501d2d83febd59fea1e727d38"
+  end
 end
